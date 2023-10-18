3f0e3635681971010b9bd58083907e184e5f7ed5
shoxrux021 <148335201+shoxrux021@users.noreply.github.com>
2023-10-18 15:33:59 +0500

    234567890-

---
+++ main.py
@@ -0,0 +1,103 @@
+ # 1)# print("number")
+# number = int(input("number:"))
+
+
+# if number > 0:
+#     print("Musbat son")
+# elif number < 0:
+#     print("Manfiy son")
+# else:
+#     print("Nol")
+ 
+
+# 2) # print("num")
+# num = int(input("sonni kiriting"))
+
+# if num % 2 == 0:
+#     print("Juft son")
+# else:
+#     print("Toq son")
+
+
+# 3)# print("name")
+# name = input("ismingizni kiriting:")
+
+# if name == "Shoxrux" or name == "Temur":
+#     print("Salom Shoxrux yoki Temur!")
+# else:
+#     print("Salom, kim siz?")
+
+
+# 4)# print("day")
+# day = input("Kunni kiriting:")
+
+# if day == "Dushanba":
+#     print("Bugun ish kuni")
+# elif day == "Shanba" or day == "Yakshanba":
+#     print("Bugun dam olish kuni")
+# else:
+#     print("Qolgan kunlar uchun kundizilarni to'ldiring")
+
+
+# 5) # num1 = 5
+# num2 = 10
+
+# if num1 > num2:
+#     print("Birinchi son katta")
+# else:
+#     print("Ikkinchi son katta yoki ular teng")
+
+
+# 6) # age = 20
+
+# if age < 13:
+#     print("Yoshingiz kichik")
+# elif age < 20:
+#     print("Yoshingiz o'rtacha")
+# else:
+#     print("Yoshingiz katta")
+
+
+# 7) # num = 15
+
+# result = "Juft son" if num % 2 == 0 else "Toq son"
+# print(result)
+
+
+# 8) # fruit = "olma"
+
+# match fruit:
+#     case "olma":
+#         print("Bu olma")
+#     case "banan":
+#         print("Bu banan")
+#     case "anor":
+#         print("Bu anor")
+#     case _:
+#         print("Bu nima?")
+
+
+# 9) # num = 5
+
+# match num:
+#     case 1:
+#         print("Bu 1")
+#     case 2:
+#         print("Bu 2")
+#     case 3, 4, 5:
+#         print("Bu 3, 4 yoki 5")
+#     case _:
+#         print("Bu boshqa son")
+
+
+# 10) # text = "salom"
+
+# match len(text):
+#     case 1:
+#         print("Bu bitta harf")
+#     case 3:
+#         print("Bu uchta harf")
+#     case 5:
+#         print("Bu beshta harf")
+#     case _:
+#         print("Bu boshqa uzunlik")
